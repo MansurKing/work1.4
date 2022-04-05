8d30b1763e6305b6e53503fee4864ea404834dc6
MansurKing <102825225+MansurKing@users.noreply.github.com>
2022-04-05 19:06:36 +0300

    init

---
+++ work1.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace work1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int imagesInAlbum = 52;
+            int groupOfImages = 3;
+
+            int fullRowsCount = imagesInAlbum / groupOfImages;
+            int remainsImagesCount = imagesInAlbum % groupOfImages;
+
+            Console.WriteLine($"Заполненных рядов в сумме получилось = {fullRowsCount}");
+            Console.WriteLine($"Оставшееся кол-во картинок, которые не вошли в ряд = {remainsImagesCount}");
+        }
+    }
+}
